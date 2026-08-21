bc7a7277be8602761b461372698f40d9ffcbaf9b
bsam50 <bsam75674@gmail.com>
2026-08-22 00:38:42 +0300

    إضافة ملفات البوت
    رفع الملفات الأساسية للبوت وتجهيز المستودع للنشر على Railway

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$TOKEN = "8937334713:AAHbK50bbjg7ryrYDM4K_SzrfRq7Yb8FICA "; // bot token
+$botUsername = "ggguuiii_bot "; // bot username without @
+
+/*
+$databaseHost = "localhost";
+$databaseName = ""; // db name
+$databaseUsername = ""; // db username
+$databasePassword = ""; // db password
+*/
+
+$sudoID = "8476500086 "; // sudo id
+
+date_default_timezone_set("Asia/Riyadh");
