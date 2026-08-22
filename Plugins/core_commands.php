db85f129e2a9cbab3d1b0f73128b83dcd33ce757
bsam50 <bsam75674@gmail.com>
2026-08-23 00:33:52 +0300

    Update core_commands.php

--- Plugins/core_commands.php
+++ Plugins/core_commands.php
@@ -7,3 +7,18 @@ function command_ping(){
 function command_start(){
     sendMessage("test 11 test");
 }
+
+function command_kick(){
+    global $chatId, $messageData;
+
+    if (!isset($messageData["reply_to_message"]["from"]["id"])) {
+        sendMessage("⚠️ يجب أن ترد على رسالة العضو الذي تريد طرده.");
+        return;
+    }
+
+    $userId = $messageData["reply_to_message"]["from"]["id"];
+
+    $result = kickChatMember($chatId, $userId);
+
+    sendMessage("✅ تم طرد العضو.");
+}
